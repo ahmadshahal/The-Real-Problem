1e4062f0a127492adb6390f6f65cca31a2f75a28
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-12-24 12:02:42 +0300

    Added bus and taxi instead of busses and taxis

--- src/domain/models/Road.java
+++ src/domain/models/Road.java
@@ -2,15 +2,15 @@ package domain.models;
 
 public class Road {
     private final int distance;
-    private final Bus[] buses;
-    private final Taxi[] taxis;
+    private final Bus bus; // Defaults to null.
+    private final Taxi taxi; // Defaults to null.
     private final Station destination;
     private final Station source;
 
-    public Road(int distance, Bus[] buses, Taxi[] taxis, Station destination, Station source) {
+    public Road(int distance, Bus bus, Taxi taxi, Station destination, Station source) {
         this.distance = distance;
-        this.buses = buses;
-        this.taxis = taxis;
+        this.bus = bus;
+        this.taxi = taxi;
         this.destination = destination;
         this.source = source;
     }
@@ -19,19 +19,19 @@ public class Road {
         return distance;
     }
 
-    public Bus[] getBuses() {
-        return buses;
-    }
-
-    public Taxi[] getTaxis() {
-        return taxis;
-    }
-
     public Station getDestination() {
         return destination;
     }
 
     public Station getSource() {
         return source;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public Taxi getTaxi() {
+        return taxi;
     }
 }
