8feaf36aef3893a0f6f7356670857c509db1ad93
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-12-24 11:41:09 +0300

    Added roads to Station

--- src/domain/models/Road.java
+++ src/domain/models/Road.java
@@ -4,11 +4,15 @@ public class Road {
     private final int distance;
     private final Bus[] buses;
     private final Taxi[] taxis;
+    private final Station destination;
+    private final Station source;
 
-    public Road(int distance, Bus[] buses, Taxi[] taxis) {
+    public Road(int distance, Bus[] buses, Taxi[] taxis, Station destination, Station source) {
         this.distance = distance;
         this.buses = buses;
         this.taxis = taxis;
+        this.destination = destination;
+        this.source = source;
     }
 
     public int getDistance() {
@@ -21,5 +25,13 @@ public class Road {
 
     public Taxi[] getTaxis() {
         return taxis;
+    }
+
+    public Station getDestination() {
+        return destination;
+    }
+
+    public Station getSource() {
+        return source;
     }
 }
--- src/domain/models/Station.java
+++ src/domain/models/Station.java
@@ -6,12 +6,14 @@ public class Station {
     private final boolean isFinal;
     private final int taxiWaitingTime;
     private final int busWaitingTime;
+    private final Road[] roads;
 
-    public Station(int id, boolean isFinal, int taxiWaitingTime, int busWaitingTime) {
+    public Station(int id, boolean isFinal, int taxiWaitingTime, int busWaitingTime, Road[] roads) {
         this.id = id;
         this.isFinal = isFinal;
         this.taxiWaitingTime = taxiWaitingTime;
         this.busWaitingTime = busWaitingTime;
+        this.roads = roads;
     }
 
     public boolean isFinal() {
@@ -32,5 +34,9 @@ public class Station {
         if (o == null || getClass() != o.getClass()) return false;
         Station station = (Station) o;
         return id == station.id;
+    }
+
+    public Road[] getRoads() {
+        return roads;
     }
 }
